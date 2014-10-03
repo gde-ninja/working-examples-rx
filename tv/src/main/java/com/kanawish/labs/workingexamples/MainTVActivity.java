0207467eecb80f2af6363452a44eb79ce7134677
Etienne Caron <etienne.caron@gmail.com>
2014-10-02 22:35:19 -0400

    Initial commit, I only tweak .gitignore to ignore all .iml files.

---
+++ tv/src/main/java/com/kanawish/labs/workingexamples/MainTVActivity.java
@@ -0,0 +1,13 @@
+package com.kanawish.labs.workingexamples;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainTVActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_tv);
+    }
+}
