2be064099841cadc1bfadf756788636a37d7190f
Etienne Caron <etienne.caron@gmail.com>
2014-10-03 22:25:51 -0400

    Added the Lorem class and example text in the assets folder. Added code in the Lorem class to load the text file asynchronously, using Rx Java. The code used to load the file off disk will be a prime candidate to demo the use of Apache commons-io package. (Future blog post) Updated res files (layouts, strings) as needed.

--- mobile/src/main/java/com/kanawish/labs/workingexamples/MainActivity.java
+++ mobile/src/main/java/com/kanawish/labs/workingexamples/MainActivity.java
@@ -2,17 +2,78 @@ package com.kanawish.labs.workingexamples;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.text.InputType;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.kanawish.labs.workingexamples.model.Lorem;
+
+import rx.android.schedulers.AndroidSchedulers;
+import rx.functions.Action1;
 
 
 public class MainActivity extends Activity {
 
-    @Override
+	private static final String TAG = MainActivity.class.getSimpleName();
+
+	@Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
+
+		final EditText editText = (EditText)findViewById(R.id.editText);
+		// To avoid getting red-underlined words in the text, add this input type flag.
+		int inputType = editText.getInputType() | InputType.TYPE_TEXT_FLAG_NO_SUGGESTIONS;
+		// Re-assign the updated input type flag.
+		editText.setInputType(inputType);
+
+		Button loadButton = (Button) findViewById(R.id.loadButton);
+		// On click of the button, triggers the editText loading process.
+		loadButton.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				/*
+				 * The thing of note here is how
+				 * - We're assigning the desired 'observing' thread
+				 * - We're handling onNext/onError
+				 */
+				Lorem.loadLorem(MainActivity.this)
+					// This makes sure onNext,onError and onComplete are called from the main thread.
+					.observeOn(AndroidSchedulers.mainThread())
+					// Subscribing will start the loading process.
+					.subscribe(
+							// onNext will be called for each lorem loaded. (1 only in this example)
+							new Action1<Lorem>() {
+								@Override
+								public void call(Lorem lorem) {
+									editText.setText(lorem.getLoremText());
+								}
+							},
+							// onError will be called if an error occurs.
+							new Action1<Throwable>() {
+								@Override
+								public void call(Throwable throwable) {
+									String msg = getString(R.string.error_loading_lorem);
+									Log.v(TAG, msg, throwable);
+									editText.setText(msg);
+								}
+							}
+					);
+			}
+		});
+
+		Button clearButton = (Button) findViewById(R.id.clearButton);
+		clearButton.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				editText.setText("");
+			}
+		});
+	}
 
 
     @Override
---
+++ mobile/src/main/java/com/kanawish/labs/workingexamples/model/Lorem.java
@@ -0,0 +1,101 @@
+package com.kanawish.labs.workingexamples.model;
+
+import android.content.Context;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.io.UnsupportedEncodingException;
+
+import rx.Observable;
+import rx.Subscriber;
+import rx.android.schedulers.AndroidSchedulers;
+import rx.schedulers.Schedulers;
+
+/**
+ * Created by etiennecaron on 2014-10-02.
+ */
+public class Lorem {
+
+	private String loremText ;
+
+	/**
+	 * Returns an Observable that will load one file off the assets directory. (lorem.txt)
+	 *
+	 * Note below how the Observable is configured to be subscribed on the io thread.
+	 *
+	 * @param context
+	 * @return
+	 */
+	public static Observable<Lorem> loadLorem(final Context context) {
+		return Observable.create(
+			new Observable.OnSubscribe<Lorem>() {
+				@Override
+				public void call(Subscriber<? super Lorem> subscriber) {
+					// Not a fan of the double-try-catch here.
+					// TODO: Change example to use apache commons-io.
+					try {
+						// Get an input stream that points to the asset.
+						InputStream is = context.getAssets().open("lorem.txt");
+						try {
+							// Create lorem instance.
+							Lorem lorem = new Lorem();
+							// Load the text from the input stream.
+							lorem.loremText = slurp(is, 2048);
+
+							// Call the subscriber with the new lorem.
+							subscriber.onNext(lorem);
+
+						} catch (IOException e) {
+							// Forward exceptions to onError()
+							subscriber.onError(e);
+						} finally {
+							// Making sure to close the input stream.
+							is.close();
+						}
+					} catch (IOException e) {
+						// Forward exceptions to onError()
+						subscriber.onError(e);
+					}
+
+					// Our job is done.
+					subscriber.onCompleted();
+				}
+			})
+			// We subscribe on io schedulers since we're I/O limited. (i.e. loading content on disk)
+			.subscribeOn(Schedulers.io());
+	}
+
+	public String getLoremText() {
+		return loremText;
+	}
+
+	/**
+	 * From http://stackoverflow.com/questions/309424/read-convert-an-inputstream-to-a-string
+	 *
+	 * @param is
+	 * @param bufferSize
+	 * @return the read String
+	 */
+	public static String slurp(final InputStream is, final int bufferSize)
+			throws UnsupportedEncodingException, IOException
+	{
+		final char[] buffer = new char[bufferSize];
+		final StringBuilder out = new StringBuilder();
+		final Reader in = new InputStreamReader(is, "UTF-8");
+		try {
+			for (;;) {
+				int rsz = in.read(buffer, 0, buffer.length);
+				if (rsz < 0)
+					break;
+				out.append(buffer, 0, rsz);
+			}
+		}
+		finally {
+			in.close();
+		}
+		return out.toString();
+	}
+}
